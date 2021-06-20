43b6d9ee1d07479af5ce62513b5174e15251afbc
Alessio <alex.robassomero@gmail.com>
2021-06-20 21:38:06 +0200

    Fixed incorrect conversion of tables that were in ROM and simplified/cleaned a bit of code.

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import re
 from cchardet import UniversalDetector
 from enum import IntEnum
+from typing import Tuple
 
 
 bwram_defines = """macro define_bwram(addr, bwram)
@@ -22,11 +23,12 @@ else
     !save_mem = $700000
 endif
 """
-special_addr_list = [8366864, 8366876, 8366888, 8366900, 8367006, 8366912, 8366924, 8366936, 8366948, 8367104,
-                     8367112, 8367120, 158, 170, 182, 194, 216, 228, 5320, 5332, 5344, 5356, 5368, 5380, 5392, 5404,
-                     5416, 5428, 5440, 5452, 5464, 5476, 5488, 5500, 5512, 5524, 5536, 5548, 5560, 5572, 5584, 5596,
-                     5610, 5622, 5634, 5646, 5658, 5670, 5682, 5694, 5706, 5718, 5730, 5742, 5754, 5766, 6252, 6267,
-                     6415, 6456, 8367872, 8150, 8162]
+sprite_addr_list = [0x7fab10, 0x7fab1c, 0x7fab28, 0x7fab34, 0x7fab9e, 0x7fab40, 0x7fab4c, 0x7fab58, 0x7fab64, 0x7fac00,
+                    0x7fac08, 0x7fac10, 0x9e, 0xaa, 0xb6, 0xc2, 0xd8, 0xe4, 0x14c8, 0x14d4, 0x14e0, 0x14ec, 0x14f8,
+                    0x1504, 0x1510, 0x151c, 0x1528, 0x1534, 0x1540, 0x154c, 0x1558, 0x1564, 0x1570, 0x157c, 0x1588,
+                    0x1594, 0x15a0, 0x15ac, 0x15b8, 0x15c4, 0x15d0, 0x15dc, 0x15ea, 0x15f6, 0x1602, 0x160e, 0x161a,
+                    0x1626, 0x1632, 0x163e, 0x164a, 0x1656, 0x1662, 0x166e, 0x167a, 0x1686, 0x186c, 0x187b, 0x190f,
+                    0x1938, 0x7faf00, 0x1fd6, 0x1fe2]
 
 
 class WordType(IntEnum):
@@ -91,7 +93,7 @@ def convert(asmfile, opt, verbose, stdout) -> None:
                                        f'\tif read1($00FFD5) == $23\n\t\t{define_found.group(0)}\n\telse\n\t\t'
                                        f'{define_found.group(0).split("=")[0]}= {"$16" if is_hex else "22"}\n\tendif\n',
                                        encoding=encoding))
-                elif int(define_found.group(1).replace('$', '0x'), 16) in special_addr_list and is_hex:
+                elif int(define_found.group(1).replace('$', '0x'), 16) in sprite_addr_list and is_hex:
                     stdout.write(bytes(f'There is define {define_found.group(0)} at line {index} which is a sprite '
                                        f'address, usually replacing the $ with ! works in most tools, it didn\'t get '
                                        f'converted automatically because it might not be necessary to do so, make sure '
@@ -177,16 +179,33 @@ def convert(asmfile, opt, verbose, stdout) -> None:
     stdout.write(bytes(f'Processed file {asmfile}\nTotal conversions: {tot_conversions}\n\n\n', encoding=encoding))
 
 
-def process_word(word, stdout, encoding, index, splitted, comma_index):
-    requires_manual_conversion = False
-    converted = True
-    bwram_define_needed = False
-    add_dp = False
-    bwram_remapped_list = [0x7F9A7B, 0x7027FF]          # Wiggler's segment buffer, Expansion area planned for SMW hacks
+def check_bwram(word: str) -> Tuple[str, bool]:
+    bwram_word = int(word, 16)
+    bwram_remapped_list = (0x7F9A7B, 0x7027FF)          # Wiggler's segment buffer, Expansion area planned for SMW hacks
     map16_lo_by = (0x7EC800, 0x7EFFFF)                # Map16 low byte plus Overworld related data.
     map16_hi_by = (0x7FC800, 0x7FFFFF)                # Map16 high byte.
     save_mem = (0x700000, 0x7007FF)                # Original save memory (2 kB big). Not everything is used
     bwram_list = [map16_lo_by, map16_hi_by, save_mem]
+    bwram_indexes = [baddr_begin <= bwram_word <= baddr_end for baddr_begin, baddr_end in bwram_list]
+    subs = ['map16_lo_by', 'map16_hi_by', 'save_mem']
+    if any(bwram_indexes):
+        true_index = bwram_indexes.index(True)
+        sub = f'${bwram_word:6X}&$00FFFF|!{subs[true_index]}'
+        return sub, True
+    elif bwram_word in bwram_remapped_list:
+        sub = f'!{bwram_word:6X}'
+        return sub, True
+    return word, False
+
+
+def check_if_shortable():
+    pass
+
+
+def process_word(word, stdout, encoding, index, splitted, comma_index):
+    requires_manual_conversion = False
+    converted = True
+    add_dp = False
     if comma_index != -1:
         if len(word) == 4 and (splitted[comma_index+1] == 'y' or splitted[comma_index+1] == 'x') \
                 and word[:2] == '00':
@@ -194,34 +213,33 @@ def process_word(word, stdout, encoding, index, splitted, comma_index):
     if word.startswith('8') and len(word) == 6:
         word = word.replace('8', '0', 1)
     try:
-        int(word, 16)
+        numeric_word = int(word, 16)  # if it's not a valid hex numeric value, go away
     except ValueError as e:
         raise e
-    # check if it's a dumb bwram remapped address
-    bwram_word = int(word, 16) if len(word) == 6 else int('7E' + word, 16)
-    if any(baddr[0] <= bwram_word <= baddr[1] for baddr in bwram_list):
-        subs = [f'${bwram_word:6X}&$00FFFF|!map16_lo_by', f'${bwram_word:6X}&$00FFFF|!map16_hi_by',
-                f'${bwram_word:6X}&$00FFFF|!save_mem']
-        bwram_define_needed = True
-        for i in range(3):
-            if bwram_list[i][0] <= bwram_word <= bwram_list[i][1]:
-                word = subs[i]
-                break
-    elif bwram_word in bwram_remapped_list:
-        bwram_define_needed = True
-        word = f'!{bwram_word:6X}'
-    elif int(word, 16) in special_addr_list:  # if special address, use define
+
+    bwram_define_needed = False
+    if len(word) == 6:
+        word, bwram_define_needed = check_bwram(word)
+    if bwram_define_needed:
+        return word, bwram_define_needed, converted, requires_manual_conversion
+
+    if numeric_word in sprite_addr_list:  # if sprite address, use define
         word = '!' + (f'{int(word, 16):X}' if not add_dp else f'{int(word, 16):X}|!dp')
-    elif len(word) == 6 and (0x000000 <= int(word, 16) <= 0x0FFFFF):  # if rom, add !bank
+    elif len(word) == 6 and (0x000000 <= numeric_word <= 0x0FFFFF):  # if rom, add !bank
         word = '$' + word + '|!bank'
-    elif len(word) == 6 and (0x7E0000 <= int(word, 16) <= 0x7E1FFF):
-        word = f'(${word}&$FFFF)|!bankA'
+    elif len(word) == 6 and (0x7E0000 <= numeric_word <= 0x7E1FFF):
+        try:
+            short_word = word[2:]
+            spr_index = sprite_addr_list.index(int(short_word, 16))
+            word = f'!{sprite_addr_list[spr_index]:X}'
+        except ValueError:
+            word = f'(${word}&$FFFF)|!bankA'
     elif len(word) == 2:  # if direct page, ignore
         converted = False
         word = '$' + word
-    elif 0x0100 <= int(word, 16) <= 0x1FFF:  # else, use |!addr
+    elif 0x0100 <= numeric_word <= 0x1FFF:  # else, use |!addr
         word = '$' + word + '|!addr'
-    elif 0x0000 <= int(word, 16) <= 0x00FF:
+    elif 0x0000 <= numeric_word <= 0x00FF:
         word = '$' + word + '|!dp'
     else:  # if out of range, ignore
         converted = False
