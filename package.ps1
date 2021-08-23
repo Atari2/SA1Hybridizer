a50e3b45597361d3e1ab83a23a281ebd63b20742
Atari2 <alex.robassomero@gmail.com>
2021-08-23 11:04:40 +0200

    Redirect all outputs to file.

--- package.ps1
+++ package.ps1
@@ -1,7 +1,6 @@
 py -m pip install --upgrade pip
-py -m pip install -r requirements.txt | Out-File where_installed.txt
+py -m pip install -r requirements.txt *> where_installed.txt
 $pyinstaller_loc = Select-String -Path where_installed.txt -Pattern "are installed in '(.+)'" -AllMatches | ForEach-Object {$_.Matches[0].Groups[1].Value}
-$pyinstaller_loc
 $pyinstaller_loc = $pyinstaller_loc + "\pyinstaller.exe"
 Invoke-Expression "$pyinstaller_loc --clean --onefile --noupx sa1hybridizer.py"
 Copy-Item conv_defines.asm dist\conv_defines.asm
