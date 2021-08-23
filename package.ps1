4762a2762af3b1b122ca5092421148e127cc3b86
Atari2 <alex.robassomero@gmail.com>
2021-08-23 11:53:34 +0200

    ugh

--- package.ps1
+++ package.ps1
@@ -1,5 +1,6 @@
 py -m pip install --upgrade pip
-$pyinstaller_loc = py -m pip install -r requirements.txt | Select-String -Pattern "are installed in '(.+)'" -AllMatches | ForEach-Object {$_.Matches[0].Groups[1].Value}
+& cmd /c "py -m pip install -r requirements.txt 1>&2" 2>&1 | ForEach-Object{ "$_" } | Tee-Object where_installed.txt 
+$pyinstaller_loc = Select-String -Path where_installed.txt -Pattern "are installed in '(.+)'" -AllMatches | ForEach-Object {$_.Matches[0].Groups[1].Value}
 $pyinstaller_loc = $pyinstaller_loc + "\pyinstaller.exe"
 Invoke-Expression "$pyinstaller_loc --clean --onefile --noupx sa1hybridizer.py"
 Copy-Item conv_defines.asm dist\conv_defines.asm
