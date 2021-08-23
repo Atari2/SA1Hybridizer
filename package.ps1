06525e92836dbaeb5e03f9a072c26eeded0ac16c
Atari2 <alex.robassomero@gmail.com>
2021-08-23 11:56:42 +0200

    Last try before I go crazy.

--- package.ps1
+++ package.ps1
@@ -1,8 +1,10 @@
 py -m pip install --upgrade pip
-& cmd /c "py -m pip install -r requirements.txt 1>&2" 2>&1 | ForEach-Object{ "$_" } | Tee-Object where_installed.txt 
-$pyinstaller_loc = Select-String -Path where_installed.txt -Pattern "are installed in '(.+)'" -AllMatches | ForEach-Object {$_.Matches[0].Groups[1].Value}
-$pyinstaller_loc = $pyinstaller_loc + "\pyinstaller.exe"
-Invoke-Expression "$pyinstaller_loc --clean --onefile --noupx sa1hybridizer.py"
+# & cmd /c "py -m pip install -r requirements.txt 1>&2" 2>&1 | ForEach-Object{ "$_" } | Tee-Object where_installed.txt 
+# $pyinstaller_loc = Select-String -Path where_installed.txt -Pattern "are installed in '(.+)'" -AllMatches | ForEach-Object {$_.Matches[0].Groups[1].Value}
+# $pyinstaller_loc = $pyinstaller_loc + "\pyinstaller.exe"
+# Invoke-Expression "$pyinstaller_loc --clean --onefile --noupx sa1hybridizer.py"
+py -m pip install -r requirements.txt
+C:\hostedtoolcache\windows\Python\3.9.6\x64\Scripts\pyinstaller.exe --clean --onefile --noupx sa1hybridizer.py
 Copy-Item conv_defines.asm dist\conv_defines.asm
 Copy-Item convert_main.py dist\convert_main.py
 Copy-Item converter.py dist\converter.py
