fc9cf448d136f2ec7cbe560ce30faa1172604ea5
Atari2 <alex.robassomero@gmail.com>
2021-08-23 11:20:26 +0200

    Another try, again.

--- package.ps1
+++ package.ps1
@@ -1,6 +1,5 @@
 py -m pip install --upgrade pip
-py -m pip install -r requirements.txt 3>&1 2>&1 > where_installed.txt
-$pyinstaller_loc = Select-String -Path where_installed.txt -Pattern "are installed in '(.+)'" -AllMatches | ForEach-Object {$_.Matches[0].Groups[1].Value}
+$pyinstaller_loc = py -m pip install -r requirements.txt | Select-String -Pattern "are installed in '(.+)'" -AllMatches | ForEach-Object {$_.Matches[0].Groups[1].Value}
 $pyinstaller_loc = $pyinstaller_loc + "\pyinstaller.exe"
 Invoke-Expression "$pyinstaller_loc --clean --onefile --noupx sa1hybridizer.py"
 Copy-Item conv_defines.asm dist\conv_defines.asm
@@ -8,7 +7,7 @@ Copy-Item convert_main.py dist\convert_main.py
 Copy-Item converter.py dist\converter.py
 Copy-Item sa1hybridizer.py dist\sa1hybridizer.py
 Set-Location dist
-Compress-Archive convert_main.py converter.py sa1hybridizer.py src.zip
-Compress-Archive sa1hybridizer.exe src.zip conv_defines.asm SA1Hybridizer.zip
+Compress-Archive -Path convert_main.py,converter.py,sa1hybridizer.py -DestinationPath src.zip
+Compress-Archive -Path sa1hybridizer.exe,src.zip,conv_defines.asm -DestinationPath SA1Hybridizer.zip
 Set-Location ..
 Move-Item dist\SA1Hybridizer.zip SA1Hybridizer.zip
